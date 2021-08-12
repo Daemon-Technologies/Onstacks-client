3790bbe4f0ebcbea30976c4e5c9bbb3d3898b174
Mina Fayek <minaf65@gmail.com>
2021-08-12 21:39:35 +0200

    Remove unused import

--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "react-google-charts";
 import useWindowDimensions from "../../hooks/useWindowDimension";
 import { randomColorGenerator } from "../../utils/helper";
