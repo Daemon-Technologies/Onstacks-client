a170bc6c2f5911c2dc130ed7159c6c86e3e2cd5f
philemonoo <87370974+philemonoo@users.noreply.github.com>
2022-05-25 18:16:04 +0200

    Update Watchlist.tsx

--- src/screens/Watchlist.tsx
+++ src/screens/Watchlist.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 // eslint-disable-next-line
 import React, { useEffect, useState } from "react";
